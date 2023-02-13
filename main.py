daae96eed4eada00a50da2d4c7982540b005b2aa
Michael Edoror <medoror@gmail.com>
2023-02-12 19:13:11 -0600

    Finish scene build

--- main.py
+++ main.py
@@ -194,9 +194,42 @@ def create_scene():
     left_wall.material = floor.material
     world.objects.append(left_wall)
 
-    camera = Camera(100, 50, math.pi/3)
 
-    camera.transform = view_transforfmation(Point(0,1.5,-5), Point(0,1,0), Vector(0,1,0))
+    right_wall = Sphere()
+    right_wall.transform = TransformationBuilder().translate(0,0,5).rotate_y(math.pi/4).rotate_x(math.pi/2).scale(10,0.01,10).build()
+    right_wall.material = floor.material
+    world.objects.append(right_wall)
+
+
+    middle = Sphere()
+    middle.transform = translation(-0.5, 1, 0.5)
+    middle.material = Material()
+    middle.material.color = Color(0.1, 1, 0.5)
+    middle.material.diffuse = 0.7
+    middle.material.specular = 0.3
+    world.objects.append(middle)
+
+    right = Sphere()
+    right.transform = translation(1.5, 0.5, -0.5) * scaling(0.5, 0.5, 0.5)
+    right.material = Material()
+    right.material.color = Color(0.5, 1, 0.1)
+    right.material.diffuse = 0.7
+    right.material.specular = 0.3
+    world.objects.append(right)
+
+
+    left = Sphere()
+    left.transform = translation(-1.5, 0.33, -0.75) * scaling(0.33, 0.33, 0.33)
+    left.material = Material()
+    left.material.color = Color(1, 0.8, 0.1)
+    left.material.diffuse = 0.7
+    left.material.specular = 0.3
+    world.objects.append(left)
+
+
+    camera = Camera(100, 50, math.pi/2)
+
+    camera.transform = view_transforfmation(Point(-5,1.5,-5), Point(0,1,0), Vector(0,1,0))
     
     canvas = render(camera, world)
     canvas_to_ppm(canvas)
